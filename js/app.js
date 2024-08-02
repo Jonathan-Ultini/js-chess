a00a02e343e02e4b402923e7d32a6866a78b160a
Jonathan-Ultini <jonathanultini@gmail.com>
2024-08-02 15:56:27 +0200

    add comment

--- js/app.js
+++ js/app.js
@@ -1,11 +1,11 @@
+// Selezione degli elementi del DOM
 const gameBoard = document.querySelector("#gameboard");
-
 const playerDisplay = document.querySelector("#player");
-
 const infoDisplay = document.querySelector("#info-display");
 
-const width = 8;
+const width = 8; // Larghezza della scacchiera
 
+// Array con la disposizione iniziale dei pezzi sulla scacchiera
 const startPieces = [
   rook, knight, bishop, queen, king, bishop, knight, rook,
   pawn, pawn, pawn, pawn, pawn, pawn, pawn, pawn,
@@ -17,31 +17,34 @@ const startPieces = [
   rook, knight, bishop, queen, king, bishop, knight, rook
 ];
 
+// Funzione per creare la scacchiera
 function createBoard() {
   startPieces.forEach((startPiece, i) => {
     const square = document.createElement('div');
     square.classList.add('square');
-    square.innerHTML = startPiece;
-    square.setAttribute('square-id', i);
-    //square.classList.add('beige');
-    const row = Math.floor(63 - i / 8) + 1;
+    square.innerHTML = startPiece; // Inserisce il pezzo nella casella
+    square.setAttribute('square-id', i); // Imposta un attributo con l'indice della casella
 
+    // Determina il colore della casella basandosi sulla posizione
+    const row = Math.floor((63 - i) / 8) + 1;
     if (row % 2 === 0) {
       square.classList.add(i % 2 === 0 ? "beige" : "brown");
     } else {
       square.classList.add(i % 2 === 0 ? "brown" : "beige");
     }
 
+    // Imposta il colore del pezzo in base alla posizione iniziale
     if (i <= 15) {
       square.firstChild.firstChild.classList.add('black');
     }
-
     if (i >= 48) {
       square.firstChild.firstChild.classList.add('white');
     }
 
+    // Aggiunge la casella alla scacchiera
     gameBoard.append(square);
-  })
+  });
 }
 
+// Crea la scacchiera all'avvio
 createBoard();
